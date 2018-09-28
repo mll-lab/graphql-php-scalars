7a8bd82659fa3c6fe5b12920d4a19d8652a7bd21
spawnia <benedikt@franke.tech>
2018-09-28 12:11:37 +0200

    styleci test2

--- src/utils.php
+++ src/utils.php
@@ -11,7 +11,12 @@ namespace MLL\GraphQLScalars;
  *
  * @return bool
  */
-function canBeString($value)           : bool
+function canBeString($value)
+
+
+
+: bool
+
 {
     return $value === null
         || is_scalar($value)

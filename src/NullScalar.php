f645744410c3dcc7055ce23ccf8742067d267bf4
spawnia <benedikt@franke.tech>
2021-12-06 16:25:46 +0100

    update

--- src/NullScalar.php
+++ src/NullScalar.php
@@ -10,9 +10,9 @@ use GraphQL\Utils\Utils;
 
 class NullScalar extends ScalarType
 {
-    public $name = 'Null';
+    public string $name = 'Null';
 
-    public $description /** @lang Markdown */
+    public ?string $description /** @lang Markdown */
         = 'Always `null`. Strictly validates value is non-null, no coercion.';
 
     public function serialize($value)
@@ -37,7 +37,7 @@ class NullScalar extends ScalarType
     {
         if (! $valueNode instanceof NullValueNode) {
             // Intentionally without message, as all information is already present in the wrapped error
-            throw new Error('');
+            throw new Error();
         }
 
         return null;

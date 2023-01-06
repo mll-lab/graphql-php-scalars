e71b3d295cba3871c7198190eb6d64dc4ad48775
spawnia <benedikt@franke.tech>
2023-01-06 21:11:19 +0100

    codestyle

--- tests/NullScalarTest.php
+++ tests/NullScalarTest.php
@@ -105,7 +105,7 @@ final class NullScalarTest extends TestCase
         /** @var array{var: mixed} $json */
         $json = \Safe\json_decode(/** @lang JSON */ <<<JSON
             {
-                "var": $jsonLiteral
+                "var": ${jsonLiteral}
             }
             JSON,
             true
